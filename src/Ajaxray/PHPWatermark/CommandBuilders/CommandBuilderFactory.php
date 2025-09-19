fdf966371492da3872081655bb84c82e90e69f42
Guillaume Bordes <gu.bordes@gmail.com>
2025-09-19 11:24:33 +0200

    bugfix PDF avec mimetype application/octet-stream

--- src/Ajaxray/PHPWatermark/CommandBuilders/CommandBuilderFactory.php
+++ src/Ajaxray/PHPWatermark/CommandBuilders/CommandBuilderFactory.php
@@ -7,6 +7,8 @@ final class CommandBuilderFactory
     private const PATTERN_MIME_IMAGE = '/^image\/\w{1,4}$/';
     private const PATTERN_MIME_PDF = '/^application\/(x\-)?pdf$/';
 
+    private const PATTERN_MIME_OCTET = '/^application\/(x\-)?octet-stream$/';
+
     public static function getCommandBuilder(string $sourcePath): WatermarkCommandBuilderInterface
     {
         $mimeType = mime_content_type($sourcePath);
@@ -16,6 +18,10 @@ final class CommandBuilderFactory
         }
 
         if (preg_match(self::PATTERN_MIME_PDF, $mimeType)) {
+            return new PDFCommandBuilder($sourcePath);
+        }
+
+        if (preg_match(self::PATTERN_MIME_OCTET, $mimeType)) {
             return new PDFCommandBuilder($sourcePath);
         }
 
